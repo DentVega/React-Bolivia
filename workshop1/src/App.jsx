f4a7a9d68d05a9113ac8cbac0995d7b0a7ec7b1d
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2019-07-27 10:33:36 -0400

    Base Proyect

---
+++ workshop1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = ({ nombre }) => (
+    <div>
+      <h1>Hola { nombre }</h1>
+      <p>Cochabamba 2019</p>
+    </div>
+);
+
+export default App;
